6056b701577afe9c721f52e9fcfc19b48c92300d
aguilaramarco@yahoo.com <aguilaramarco@yahoo.com>
2020-03-16 18:28:36 -0700

    Created a folder for model classes. Added Patient and Physician classes.

---
+++ SED/app/src/main/java/com/alvarezaaronai/sed/models/Patient.java
@@ -0,0 +1,29 @@
+package com.alvarezaaronai.sed.models;
+
+public class Patient {
+
+    private String first_name;
+
+    private String last_name;
+
+    private String email;
+
+    private String patient_image;
+
+    public String getPatient_image() {
+        return patient_image;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
