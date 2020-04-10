e972e41e57923d20acb53da974c8def7690f3847
acortes7 <cortes.ana13@yahoo.com>
2020-04-09 21:01:06 -0700

    update

--- SED/app/src/main/java/com/alvarezaaronai/sed/LogIn.java
+++ SED/app/src/main/java/com/alvarezaaronai/sed/LogIn.java
@@ -1,4 +1,46 @@
 package com.alvarezaaronai.sed;
 
-public class LogIn {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LogIn extends AppCompatActivity {
+    Button logInButt;
+    EditText userId, userPw;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        logInButt = (Button)findViewById(R.id.signInButton);
+        userId = (EditText)findViewById(R.id.userID);
+        userPw = (EditText)findViewById(R.id.passwordID);
+
+        logInButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(userId.getText().toString().equals("doctor") &&
+                        userPw.getText().toString().equals("doctorPW")) {
+                    // change to doctor page instead of patient page activity
+                    Intent doctorIntent = new Intent(LogIn.this,PatientPage.class);
+                    startActivity(doctorIntent);
+                    finish();
+                }
+                else if (userId.getText().toString().equals("patient") &&
+                        userPw.getText().toString().equals("patientPW")){
+                    Intent patientIntent = new Intent(LogIn.this,PatientPage.class);
+                    startActivity(patientIntent);
+                    finish();
+                }else{
+                    System.out.println("Not valid");
+                }
+            }
+        });
+    }
 }
--- SED/app/src/main/java/com/alvarezaaronai/sed/PatientPage.java
+++ SED/app/src/main/java/com/alvarezaaronai/sed/PatientPage.java
@@ -1,4 +1,28 @@
 package com.alvarezaaronai.sed;
 
-public class PatientPage {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PatientPage extends AppCompatActivity {
+    private Button signOut, chartResults, realTime;
+    private View graphs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_page);
+
+        signOut = (Button)findViewById(R.id.patientSignOutButton);
+        chartResults= (Button)findViewById(R.id.chartResults);
+        realTime= (Button)findViewById(R.id.realTime);
+
+        graphs = findViewById(R.id.graphResults);
+
+
+    }
+
+    //TODO display graph in View when clicking on appropriate button
 }
