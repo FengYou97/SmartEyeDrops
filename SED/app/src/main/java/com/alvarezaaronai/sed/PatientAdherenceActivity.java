1a545856d894616ab18328d28fd82592ffdcff0c
aguilaramarco@yahoo.com <aguilaramarco@yahoo.com>
2020-04-10 23:50:16 -0700

    Got days of the week showing up correctly on the top x-axis.

--- SED/app/src/main/java/com/alvarezaaronai/sed/PatientAdherenceActivity.java
+++ SED/app/src/main/java/com/alvarezaaronai/sed/PatientAdherenceActivity.java
@@ -3,7 +3,9 @@ package com.alvarezaaronai.sed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.alvarezaaronai.sed.utils.AdherenceXAxisFormatter;
 import com.github.mikephil.charting.charts.ScatterChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.ScatterData;
@@ -15,6 +17,7 @@ import java.util.List;
 
 public class PatientAdherenceActivity extends AppCompatActivity {
 
+    private static final String TAG = "PatientAdherenceActivit";
 
     private List<Entry> mEntries;
     private ScatterChart mScatterChart;
@@ -35,26 +38,49 @@ public class PatientAdherenceActivity extends AppCompatActivity {
 
         // Finally, we can create our ScatterData
         ScatterData scatterData = new ScatterData(dataSetList);
+
         mScatterChart.setData(scatterData);
+
+        configureScatterChart();
+
         // IMPORTANT!! Don't forget!
         mScatterChart.invalidate(); // Refresh
 
     }
 
+    private void configureScatterChart() {
+        // Configure ScatterChart
+        mScatterChart.setTouchEnabled(false); // Disables interaction with graph
+        mScatterChart.getXAxis().setAxisMaximum(0.8f);
+        mScatterChart.getXAxis().setAxisMinimum(0.0f);
+        // Use the formatter
+        mScatterChart.getXAxis().setValueFormatter(new AdherenceXAxisFormatter());
+    }
+
+
     /**
      *  Gonna be using this to hard code some Entry values
      *  and returning a list of Entry objects.
+     *
+     *  XAxis:
+     *      Mon = 0.1
+     *      Tue = 0.2
+     *      Wed = 0.3
+     *      Thu = 0.4
+     *      Fri = 0.5
+     *      Sat = 0.6
+     *      Sun = 0.7
      */
     private List<Entry> generateEnrtries() {
         // Entry(float x, float y)
         List<Entry> entries = new ArrayList<Entry>();
 
-        Entry firstEntry = new Entry(1f,1f);
-        Entry secondEntry = new Entry(2f,2f);
-        Entry thirdEntry = new Entry(2f,2f);
+        Entry firstEntry = new Entry(0.1f,1.8f);
+        Entry secondEntry = new Entry(0.4f,2f);
+
         entries.add(firstEntry);
         entries.add(secondEntry);
-        entries.add(thirdEntry);
+
 
         return entries;
     }
